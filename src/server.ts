898431ccca78130f9fb2950d037f0c99cf57f4f8
Nycolas R. Alves <nycolasramon3@gmail.com>
2023-01-18 01:45:55 -0300

    Avanços nas rotas e db

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
-import Fastify from "fastify"
-import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
-const   = Fastify()
-const prisma = new PrismaClient();
+const app = Fastify();
 
-app.register(cors)
+app.register(cors);
+app.register(appRoutes);
 
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-})
-
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running!");
+  });
